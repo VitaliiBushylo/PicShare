48f8446573778b843a5f42f5bdb807ac9b5cfb6b
VB <vb>
2017-08-18 18:20:03 +0300

    Added requirejs package.

---
+++ PicShare/Scripts/main.js
@@ -0,0 +1,11 @@
+﻿require.config({
+    paths: {
+        jquery: 'jquery-1.9.1.min',
+        bootstrap: 'bootstrap.min',
+        jquery_md5: 'jquery.md5',
+        knockout: 'knockout-3.4.2',
+
+        templates: '../views/templates'
+    }
+
+});
