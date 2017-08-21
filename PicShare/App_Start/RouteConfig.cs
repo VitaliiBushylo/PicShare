9960eff88e0cee6a3a48b93a2244489207d2aa38
VB <vb>
2017-08-21 10:16:50 +0300

    Added TemplatesController to handle requests from text.js plugin.

--- PicShare/App_Start/RouteConfig.cs
+++ PicShare/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace PicShare
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Templates",
+                url: "Templates/{*viewName}",
+                defaults: new { controller = "Templates", action = "Page" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
